dc2951d3753e2e6e556b6385fdfddf50efdbd88b
gavy0037 <Gsinghhps@gmail.com>
2026-04-03 03:06:44 +0530

    testing q1

--- q1/q1.s
+++ q1/q1.s
@@ -1,10 +1,10 @@
 .section .text
 
 # define the offsets for our struct
-.equ NODE_VAL 0
-.equ NODE_LEFT 4
-.equ NODE_RIGHT 8
-.equ NODE_SIZE 12 
+.equ NODE_VAL, 0
+.equ NODE_LEFT, 8
+.equ NODE_RIGHT, 16
+.equ NODE_SIZE, 24
 
 .global make_node,insert,get,getAtMost
 # i will use s0 to store the root's pointer
@@ -12,27 +12,27 @@
 make_node:
     # val is palaced in a0
     addi sp,sp,-16
-    sw a0,0(sp) # save the val on stack
-    sw ra,4(sp)
+    sd a0,0(sp) # save the val on stack
+    sd ra,8(sp)
     li  a0,NODE_SIZE
     call malloc
     # now i have base address of node in the a0
-    lw t0,0(sp)
-    lw ra,4(sp)
+    ld t0,0(sp)
+    ld ra,8(sp)
     addi sp,sp,16
 
-    sw  t0,NODE_VAL(a0)
+    sd  t0,NODE_VAL(a0)
     li  t0,0
-    sw  t0,NODE_LEFT(a0)
-    sw  t0,NODE_RIGHT(a0)
+    sd  t0,NODE_LEFT(a0)
+    sd  t0,NODE_RIGHT(a0)
 
     ret # as the base address is already in a0 , so just return
 
 insert:
     # two parameters , first is root's address and second is value of newnode
     addi sp , sp , -16
-    sw a0,0(sp) # save root's address
-    sw ra,4(sp)
+    sd a0,0(sp) # save root's address
+    sd ra,8(sp)
 
 
     li  t0,0
@@ -40,61 +40,61 @@ insert:
     
     mv a0,a1
     call make_node
-    lw ra,4(sp)
+    ld ra,8(sp)
     addi sp,sp,16
     # no need for a0 , as it's anyways zero
     ret
 
     notNULL:
-    lw  t0,NODE_VAL(a0)
+    ld  t0,NODE_VAL(a0)
     ble t0,a1,GO_RIGHT
 
     # for value insertion in left subtree
     #call insert , we will get a value in a0 , that is the returned thing , and thatshould be placed in root's left
 
-    lw  a0,NODE_LEFT(a0) # load the left's adress in first argument.
+    ld  a0,NODE_LEFT(a0) # load the left's adress in first argument.
     call insert
-    lw t0,0(sp)
-    sw  a0,NODE_LEFT(t0)
+    ld t0,0(sp)
+    sd  a0,NODE_LEFT(t0)
 
 
-    lw  a0,0(sp)
-    lw  ra,4(sp)
+    ld  a0,0(sp)
+    ld  ra,8(sp)
     addi sp,sp,16
     ret
     
 
     GO_RIGHT:
-    lw  a0,NODE_RIGHT(a0) # load the right's adress in first argument.
+    ld  a0,NODE_RIGHT(a0) # load the right's adress in first argument.
     call insert
-    lw  t0,0(sp)
-    sw  a0,NODE_RIGHT(t0)
+    ld  t0,0(sp)
+    sd  a0,NODE_RIGHT(t0)
 
 
-    lw  a0,0(sp)
-    lw  ra,4(sp)
+    ld  a0,0(sp)
+    ld  ra,8(sp)
     addi sp,sp,16
     ret
 
 get:
     # two parameters , first is root's address and second is value of newnode
     addi sp , sp , -16
-    sw ra,0(sp)
+    sd ra,0(sp)
 
 
     li  t0,0
-    bne a0,t0,notNULL
+    bne a0,t0,notNULLget
 
-    lw ra,0(sp)
+    ld ra,0(sp)
     addi sp,sp,16
     ret # a0 is already 0 , so just return root
 
-    notNULL:
+    notNULLget:
     
-    lw t0,NODE_VAL(a0)
+    ld t0,NODE_VAL(a0)
     bne t0,a1,leftOrRight
 
-    lw ra,0(sp)
+    ld ra,0(sp)
     addi sp,sp,16
     ret # a0 already contains the root's address
     
@@ -103,17 +103,17 @@ get:
 
     RIGHT:
 
-    lw a0,NODE_RIGHT(a0)
+    ld a0,NODE_RIGHT(a0)
     call get
-    lw ra,0(sp)
+    ld ra,0(sp)
     addi sp,sp,16
     ret
 
     LEFT:
 
-    lw a0,NODE_LEFT(a0)
+    ld a0,NODE_LEFT(a0)
     call get
-    lw ra,0(sp)
+    ld ra,0(sp)
     addi sp,sp,16
     ret
 
@@ -125,15 +125,15 @@ getAtMost:
     loop:
         beq a1,t1,exit
 
-        lw t2,NODE_VAL(a1)
+        ld t2,NODE_VAL(a1)
         blt a0,t2,goLeft
 
         mv t0,t2 # ans = root->val
-        lw a0,NODE_RIGHT(a1)
+        ld a1,NODE_RIGHT(a1)
         j loop
 
         goLeft:
-        lw a0,NODE_LEFT(a1)
+        ld a1,NODE_LEFT(a1)
         j loop
     exit:
     mv a0,t0
